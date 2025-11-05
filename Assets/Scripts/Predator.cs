61149eb61819638ec4e3559844a658e1e6e86010
Maj Frost Jensen <mfje@itu.dk>
2025-11-05 14:27:14 +0100

    shark now moves!!

--- Assets/Scripts/Predator.cs
+++ Assets/Scripts/Predator.cs
@@ -5,10 +5,36 @@ public class Predator : MonoBehaviour
     public GameObject manager;
     private FlockingManager flockingManager;
 
+    public float moveSpeed = 15f;
+    public Vector3 targetPosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         flockingManager = manager.GetComponent<FlockingManager>();
+    }
+
+    void Update()
+    {
+        MovePredator();
+    }
+
+    private void MovePredator()
+    {
+        Vector3 direction = (targetPosition - transform.position).normalized;
+
+        if (direction != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 2f * Time.deltaTime);
+        }
+
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, targetPosition) < 5f)
+        {
+            targetPosition = new Vector3(Random.Range(-100, 100), Random.Range(-50, 50), Random.Range(-100, 100));
+        }
     }
 
     void OnTriggerStay(Collider other)
