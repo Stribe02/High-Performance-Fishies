8247a624e559dac70066a7bc6c9297858694b0db
Maj Frost Jensen <mfje@itu.dk>
2025-11-05 12:12:10 +0100

    Fishies are now seperated into schools

--- Assets/Scripts/Predator.cs
+++ Assets/Scripts/Predator.cs
@@ -13,14 +13,23 @@ public class Predator : MonoBehaviour
 
     void OnTriggerStay(Collider other)
     {
-        flockingManager.cohesionWeight = -2f;
-        flockingManager.alignmentWeight = -0.5f;
+        Boid boid = other.GetComponent<Boid>();
+        if (boid != null) {
+            flockingManager.SetSchoolWeights(boid.schoolIndex, -3f, -1f, flockingManager.defaultAlignmentWeight);
+        }
+        //flockingManager.cohesionWeight = -2f;
+        //flockingManager.alignmentWeight = -0.5f;
     }
 
     void OnTriggerExit(Collider other)
     {
-        flockingManager.cohesionWeight = 1f;
-        flockingManager.alignmentWeight = 1f;
+        Boid boid = other.GetComponent<Boid>();
+        if (boid != null)
+        {
+            flockingManager.ResetSchoolWeights(boid.schoolIndex);
+        }
+        //flockingManager.cohesionWeight = 1f;
+        //flockingManager.alignmentWeight = 1f;
 
     }
 
