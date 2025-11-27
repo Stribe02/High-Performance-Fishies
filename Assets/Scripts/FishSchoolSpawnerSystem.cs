037531db9052fb64a08afcfaaf7a9f00954d93b4
Maj <maj@frost-jensen.dk>
2025-11-27 12:53:54 +0100

    Fish Spawning job

--- Assets/Scripts/FishSchoolSpawnerSystem.cs
+++ Assets/Scripts/FishSchoolSpawnerSystem.cs
@@ -1,39 +1,47 @@
+using System;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
 
 
+
 partial struct FishSchoolSpawner : ISystem
 {
+    BufferLookup<LinkedEntityGroup> linkedEntityGroupLookup;
+    
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<Config>();
         state.RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
+
+        linkedEntityGroupLookup = state.GetBufferLookup<LinkedEntityGroup>();
+
     }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         state.Enabled = false;
-
+        
         var config = SystemAPI.GetSingleton<Config>();
-        var ecb =
-            SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton
-            >().CreateCommandBuffer(state.WorldUnmanaged);
+        var ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
 
         for (int i = 0; i < config.NumberOfSchools; i++)
         {
-            // FISH SCHOOL HAVE THEIR OWN ENTITY FOR THEIR SCHOOL
+
             var fishSchoolEntity = state.EntityManager.CreateEntity();
             state.EntityManager.SetName(fishSchoolEntity, $"FishSchool {i}");
             state.EntityManager.AddComponent<FishSchoolAttribute>(fishSchoolEntity);
+
             DynamicBuffer<SchoolFishes> schoolBuffer = ecb.AddBuffer<SchoolFishes>(fishSchoolEntity);
             schoolBuffer.Capacity = config.FlockSize;
+
             state.EntityManager.SetComponentData<FishSchoolAttribute>(fishSchoolEntity, new FishSchoolAttribute
                 {
                     SchoolIndex = i,
@@ -47,51 +55,74 @@ partial struct FishSchoolSpawner : ISystem
             );
             var fishSchoolData = state.EntityManager.GetComponentData<FishSchoolAttribute>(fishSchoolEntity);
 
-            // URPMaterialPropertyBaseColor is a component from the Entities.Graphics package 
-            // that lets us set the rendered base color of a rendered entity.
             Color c = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-            var color = new URPMaterialPropertyBaseColor { Value = new float4(c.r, c.g, c.b, c.a) };
+            var colorc = new float4(c.r, c.g, c.b, c.a);
 
             var fishes = state.EntityManager.Instantiate(fishSchoolData.FishPrefab, fishSchoolData.FlockSize, Allocator.Persistent); //leak?
+
             schoolBuffer.EnsureCapacity(fishes.Length);
+            schoolBuffer.AddRange(fishes.Reinterpret<SchoolFishes>());
+
+            linkedEntityGroupLookup.Update(ref state);
+
+            JobHandle spawnJob = new SpawnFishJob
+            {
+                ecb = ecb,
+                fishes = fishes,
+                color = colorc,
+                linkedEntityGroups = linkedEntityGroupLookup,
+                speed = 2f,
+                schoolIndex = SystemAPI.GetComponentRW<FishSchoolAttribute>(fishSchoolEntity).ValueRW.SchoolIndex
+            }.Schedule(state.Dependency);
+            spawnJob.Complete();
+
+            fishes.Dispose();
+        }
+    }
+
+    [BurstCompile]
+    public partial struct SpawnFishJob : IJobEntity
+    {
+        public EntityCommandBuffer ecb;
+        public NativeArray<Entity> fishes;
+        public BufferLookup<LinkedEntityGroup> linkedEntityGroups;
+        public float4 color;
+        public float speed;
+        public int schoolIndex;
+        public void Execute(ref LocalTransform localTransform, ref FishAttributes fishAttributes, ref AquaticAnimalAttributes aquaticAttributes, ref URPMaterialPropertyBaseColor baseColor)
+        {
+            var ran = new Unity.Mathematics.Random(((uint)schoolIndex) + 1);
 
             foreach (var fish in fishes)
             {
-                schoolBuffer.Add(new SchoolFishes { Fish = fish });
-         
-                var transform = SystemAPI.GetComponentRW<LocalTransform>(fish);
-                ecb.AddComponent<FishAttributes>(fish);
-                
-                transform.ValueRW.Position = UnityEngine.Random.insideUnitSphere * 10;
-                ecb.AddComponent<AquaticAnimalAttributes>(fish);
-
-                var speed = SystemAPI.GetComponentRW<AquaticAnimalAttributes>(fish).ValueRW.Speed = 2f;
-                SystemAPI.GetComponentRW<FishAttributes>(fish).ValueRW.Velocity = UnityEngine.Random.insideUnitSphere.normalized * speed;
-                SystemAPI.GetComponentRW<FishAttributes>(fish).ValueRW.SchoolIndex = i;
-
-                // Every root entity instantiated from a prefab has a LinkedEntityGroup component, which
-                // is a list of all the entities that make up the prefab hierarchy (including the root).
-
-                // (LinkedEntityGroup is a special kind of component called a "DynamicBuffer", which is
-                // a resizable array of struct values instead of just a single struct.)
-                var linkedEntities = state.EntityManager.GetBuffer<LinkedEntityGroup>(fish);
-                foreach (var entity in linkedEntities)
+                ecb.AddComponent<FishAttributes>(fish, new FishAttributes
                 {
-                    // We want to set the URPMaterialPropertyBaseColor component only on the
-                    // entities that have it, so we first check.
-                    if (state.EntityManager.HasComponent<URPMaterialPropertyBaseColor>(entity.Value))
-                    {
-                        // Set the color of each entity that makes up the tank.
-                        state.EntityManager.SetComponentData(entity.Value, color);
-                    }
-                }
+                    SchoolIndex = schoolIndex,
+                    Velocity = ran.NextFloat3(0,1) * 2f
+                });
+                ecb.AddComponent<AquaticAnimalAttributes>(fish, new AquaticAnimalAttributes
+                {
+                    Speed = speed,
+                    Radius = aquaticAttributes.Radius
+                });
+                ecb.AddComponent<LocalTransform>(fish, new LocalTransform
+                {
+                    Position = ran.NextFloat3(-10,10),
+                    Rotation = localTransform.Rotation,
+                    Scale = localTransform.Scale
+                });
+
+                linkedEntityGroups.TryGetBuffer(fish, out DynamicBuffer<LinkedEntityGroup> linkedEnts);
+
+                foreach (var ent in linkedEnts)
+                {
+                    ecb.SetComponent<URPMaterialPropertyBaseColor>(ent.Value, new URPMaterialPropertyBaseColor { Value = color });
+                }  
             }
-
-            //SystemAPI.GetComponentRW<FishSchoolAttribute>(fishSchoolEntity).ValueRW.Fishes = fishes;
-            fishes.Dispose();
-
         }
     }
+
  }
+
 
 
